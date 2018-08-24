512206f027377fa377d57b9baa029e466ca275f5
Clement.ROCHER <clement.rocher@edu.ece.fr>
2018-08-24 16:57:19 +0200

    Page accueil finale

--- app/src/main/java/com/example/clementrocher/quizbl/activities/AccueilActivity.java
+++ app/src/main/java/com/example/clementrocher/quizbl/activities/AccueilActivity.java
@@ -1,9 +1,11 @@
 package com.example.clementrocher.quizbl.activities;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -17,6 +19,10 @@ public class AccueilActivity extends AppCompatActivity {
     ImageButton deconnexionButton;
     Button gameButton;
     Button classementButton;
+    Button retourButton;
+    Button decoButton;
+    Dialog dialogDeco;
+    TextView alertbox_quit_text;
 
 
     @Override
@@ -41,13 +47,6 @@ public class AccueilActivity extends AppCompatActivity {
             }
         });
 
-        deconnexionButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intentDeconnexion = new Intent(AccueilActivity.this, LoginActivity.class);
-                startActivity(intentDeconnexion);
-            }
-        });
 
         gameButton.setText("Lancer Une \nPartie !");
         gameButton.setOnClickListener(new View.OnClickListener() {
@@ -67,6 +66,36 @@ public class AccueilActivity extends AppCompatActivity {
             }
         });
 
+        deconnexionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Pop Up des conditions d'utilisations
+                dialogDeco = new Dialog(AccueilActivity.this);
+                dialogDeco.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                dialogDeco.setContentView(R.layout.deconnexion_pop_up);
+                dialogDeco.setCanceledOnTouchOutside(false);
+                alertbox_quit_text = dialogDeco.findViewById(R.id.alertbox_quit_text);
+
+                retourButton = dialogDeco.findViewById(R.id.retourButton);
+                decoButton = dialogDeco.findViewById(R.id.decoButton);
+                retourButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialogDeco.dismiss();
+                    }
+                });
+                decoButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intentDeco = new Intent(AccueilActivity.this,LoginActivity.class);
+                        startActivity(intentDeco);
+                    }
+                });
+
+                dialogDeco.show();
+            }
+        });
 
         /*
         TODO : Récuperer le nom et prénom en BDD et les afficher dans profilAffichageTextView
